e2030d35628fb72ee8bbab3046808bb459736768
Hidden Gem - Bidyut Roy <74731201+BidyutRoy2@users.noreply.github.com>
2024-08-23 13:42:41 +0600

    Update rivalz.sh

--- rivalz.sh
+++ rivalz.sh
@@ -11,7 +11,7 @@ print_command() {
 cd $HOME
 
 print_command "Installing NVM and Node..."
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
 
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 
